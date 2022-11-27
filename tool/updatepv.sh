4140a9497e41109bbe696b7ca7ecda2eaf596570
zhaowcheng <zhaowcheng@163.com>
2022-11-27 19:02:02 +0800

    update 2022-11-26 pageviews

--- tool/updatepv.sh
+++ tool/updatepv.sh
@@ -8,4 +8,4 @@ python3 ./tool/csvpv2jsonpv.py ./data/pageviews.csv ./data/pageviews.json
 git add .
 git commit -m "update `date --date="yesterday" +"%Y-%m-%d"` pageviews"
 git push
-ln -sf $TOOLDIR/../data/pageviews.json /var/www/blog.zhaowcheng.com/_site/pageviews.json
+ln -sfv $TOOLDIR/../data/pageviews.json /var/www/blog.zhaowcheng.com/_site/pageviews.json
