286a73029d889a4a1dd04416510a87f69b6623a1
zhaowcheng <zhaowcheng@163.com>
2022-11-27 18:59:33 +0800

    update 2022-11-26 pageviews

--- tool/updatepv.sh
+++ tool/updatepv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -xe
 
-TOOLDIR=$(dirname `realpath ${BASH_SOURCE[0]}`)
+BASEDIR=$(dirname `realpath ${BASH_SOURCE[0]}`/..)
 
-cd $TOOLDIR/..
+cd $BASEDIR
 python3 ./tool/nginxlog2csvpv.py /var/log/nginx/access.log.1 ./data/pageviews.csv
 python3 ./tool/csvpv2jsonpv.py ./data/pageviews.csv ./data/pageviews.json
 git add .
 git commit -m "update `date --date="yesterday" +"%Y-%m-%d"` pageviews"
 git push
-ln -svf $TOOLDIR/../data/pageviews.json /var/www/blog.zhaowcheng.com/_site/pageviews.json
+ln -svf $BASEDIR/data/pageviews.json /var/www/blog.zhaowcheng.com/_site/pageviews.json
