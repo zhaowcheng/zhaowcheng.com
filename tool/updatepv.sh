57750a2789c3b839106f56d18f0e4e6ca79cb72d
zhaowcheng <zhaowcheng@163.com>
2023-11-09 19:15:26 +0800

    update 2023-11-08 pageviews

--- tool/updatepv.sh
+++ tool/updatepv.sh
@@ -9,5 +9,5 @@ git config user.name zhaowcheng
 git config user.email zhaowcheng@163.com
 git add .
 git commit -m "update `date --date="yesterday" +"%Y-%m-%d"` pageviews"
-git push
+git push --set-upstream origin master
 ln -sfv $TOOLDIR/../data/pageviews.json /var/www/blog.zhaowcheng.com/_site/pageviews.json
